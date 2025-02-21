399697926888caaff95c9811e77a8e4aa9be3232
bernst4315 <bernst4315@gmail.com>
2025-02-21 15:04:13 -0700

    basic css, and hard code end results

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,17 @@ function App() {
   
 
   return (
-    <>
-      
-    </>
+    <div>
+      <h1>Todo List</h1>
+      <form action="" className='task-list'>
+        <div className='task-item'>
+          <input type="checkbox" className='checkbox'/>
+          <p>Todo task</p>
+          <button>Edit</button>
+          <button>Delete</button>
+        </div>
+      </form>
+    </div>
   )
 }
 
